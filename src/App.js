5c35d57751e6111548fa9ed17678b147f0dd3b51
Laviniiabenvindo <laviniiabenvindo@gmail.com>
2024-04-04 14:24:18 -0300

    Trabalhando com Props

--- src/App.js
+++ src/App.js
@@ -1,40 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
 import HelloWard from './components/HelloWord';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Lavínia emelly';
-  const newNema = name.toUpperCase();
-  const url = 'https://i1.sndcdn.com/artworks-000357909669-bveymq-t500x500.jpg';
-
-  function sum(a, b) {
-    return a + b;
-  }
-
+  const nome = 'Marcela';
   return (
     <div className="App">
-      <h1>Alterando o JSX</h1>
-      <p>Olá {newNema}</p>
-      <p>Soma: {sum(2, 2)}</p>
       <HelloWard />
-      {/* <img src={url} alt="Minha imagem"></img> */}
+      <SayMyName nome="Lavouras" />
+      <SayMyName nome="Juan" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Lavinia" idade="18" profissao="Programadora" foto="https://i1.sndcdn.com/artworks-000357909669-bveymq-t500x500.jpg" />
     </div>
   );
-}
-{
-  /* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */
 }
 export default App;
